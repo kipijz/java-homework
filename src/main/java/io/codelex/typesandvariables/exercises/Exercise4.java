4c4aab5f6cade80d717e4feb4197186a150613da
Kristaps <kristaps_png@protonmail.com>
2022-02-23 19:39:26 +0200

    finish exercise5

---
+++ src/main/java/io/codelex/typesandvariables/exercises/Exercise4.java
@@ -0,0 +1,11 @@
+package io.codelex.typesandvariables.exercises;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        String name = "Kristaps";
+        int birthYear = 1997;
+
+        System.out.println("My name is " + name + " and I was born in " + birthYear + ".");
+
+    }
+}
---
+++ src/main/java/io/codelex/typesandvariables/exercises/Exercise5.java
@@ -0,0 +1,25 @@
+package io.codelex.typesandvariables.exercises;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        String course1 = "English III";
+        String course2 = "Precalculus";
+        String course3 = "Music Theory";
+        String course4 = "Biotechnology";
+
+        String teacher1 = "Ms. Lapan";
+        String teacher2 = "Mrs. Gideon";
+        String teacher3 = "Mr. Davis";
+        String teacher4 = "Ms. Palmer";
+
+
+        System.out.println("+---------------------------------------------------+");
+
+        System.out.println("| 1 |          " + course1 + " |     " + teacher1 + " |");
+        System.out.println("| 2 |          " + course2 + " |     " + teacher2 + " |");
+        System.out.println("| 3 |          " + course3 + " |     " + teacher3 + " |");
+        System.out.println("| 4 |          " + course4 + " |     " + teacher4 + " |");
+
+        System.out.println("+---------------------------------------------------+");
+    }
+}

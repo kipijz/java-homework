b8b9d70873d732d25a0441904d6c5ab7cbfae29a
Kristaps <kristaps_png@protonmail.com>
2022-03-14 19:03:26 +0200

    finish date and time exercises

---
+++ src/main/java/io/codelex/dateandtime/exercises/exercise1/Exercise1Test.java
@@ -0,0 +1,24 @@
+package io.codelex.dateandtime.exercises.exercise1;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Exercise1Test {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input first date: ");
+        LocalDate date1 = addDate(scanner);
+
+        System.out.println("Input second date: ");
+        LocalDate date2 = addDate(scanner);
+
+        Work.calculateHours(date1, date2);
+
+    }
+
+    public static LocalDate addDate(Scanner scan) {
+        return LocalDate.parse(scan.next());
+    }
+}

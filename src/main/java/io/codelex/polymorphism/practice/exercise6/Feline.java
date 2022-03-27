dc6d3d82ecbff28f04cd04a49b2eebe1ecc3190f
Kristaps <kristaps_png@protonmail.com>
2022-03-27 17:50:49 +0300

    finish 6th exercise

---
+++ src/main/java/io/codelex/polymorphism/practice/exercise6/Feline.java
@@ -0,0 +1,7 @@
+package io.codelex.polymorphism.practice.exercise6;
+
+public abstract class Feline extends Mammal{
+    public Feline(String animalType, String animalName, double animalWeight, String livingRegion) {
+        super(animalType, animalName, animalWeight, livingRegion);
+    }
+}
---
+++ src/main/java/io/codelex/polymorphism/practice/exercise6/Tiger.java
@@ -0,0 +1,14 @@
+package io.codelex.polymorphism.practice.exercise6;
+
+public class Tiger extends Feline{
+
+    public Tiger(String animalType, String animalName, double animalWeight, String livingRegion) {
+        super(animalType, animalName, animalWeight, livingRegion);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("RAAAAAAAAAAAAAAAAAWR");
+    }
+}
+

e13190c4a495e8b9644b0a1f519a04d7572c61dc
Kristaps <kristaps_png@protonmail.com>
2022-03-28 13:30:09 +0300

    finish pull request comments

--- src/main/java/io/codelex/enums/exercise2/Game.java
+++ src/main/java/io/codelex/enums/exercise2/Game.java
@@ -1,23 +1,24 @@
 package io.codelex.enums.exercise2;
 
-import org.apache.commons.lang3.mutable.MutableInt;
-
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
+
+    private static int userWins;
+    private static int computerWins;
+    private static int numberOfTrials;
+    private static MoveChoice computerChoice;
+
+
     public static void startGame() {
         Scanner scanner = new Scanner(System.in);
-        MutableInt userWins = new MutableInt(0);
-        MutableInt computerWins = new MutableInt(0);
-        MutableInt numberOfTrials = new MutableInt(0);
         boolean beingPlayed = true;
 
         System.out.println("Let us begin...");
         while (beingPlayed) {
-            //user turn
             boolean isInputValid;
             MoveChoice userChoice;
             do {
@@ -33,14 +34,11 @@ public class Game {
             } while (!isInputValid);
 
             if (userChoice == MoveChoice.QUIT) {
-                //user quits
-                endGameResults(userWins, computerWins, numberOfTrials);
+                endGameResults();
                 beingPlayed = false;
             } else {
-                //computer turn
-                MoveChoice computerChoice = computerTurn();
-                System.out.print("My turn: " + computerChoice + "\n");
-                whoWonTurn(userChoice, computerChoice, userWins, computerWins, numberOfTrials);
+                computerTurn();
+                whoWonTurn(userChoice, computerChoice);
             }
         }
     }
@@ -58,56 +56,47 @@ public class Game {
         return userChoice;
     }
 
-    private static MoveChoice computerTurn() {
+    private static void computerTurn() {
         int computerRandomNumber = new Random().nextInt(3);
         ArrayList<MoveChoice> computerChoicesArrayList = new ArrayList<>();
         computerChoicesArrayList.add(MoveChoice.SCISSOR);
         computerChoicesArrayList.add(MoveChoice.PAPER);
         computerChoicesArrayList.add(MoveChoice.STONE);
-        return computerChoicesArrayList.get(computerRandomNumber);
+        computerChoice = computerChoicesArrayList.get(computerRandomNumber);
+        System.out.print("My turn: " + computerChoice + "\n");
     }
 
-    private static void whoWonTurn(MoveChoice userTurn, MoveChoice computerTurn, MutableInt userWins, MutableInt computerWins, MutableInt numberOfTrials) {
-        numberOfTrials.add(1);
+    private static void whoWonTurn(MoveChoice userTurn, MoveChoice computerTurn) {
+        numberOfTrials++;
         if (userTurn == computerTurn) {
             System.out.println("TIE" + "\n");
         } else if (userTurn == MoveChoice.PAPER && computerTurn == MoveChoice.SCISSOR) {
             System.out.println("Scissor cuts paper, I won!" + "\n");
-            computerWins.add(1);
+            computerWins++;
         } else if (userTurn == MoveChoice.PAPER && computerTurn == MoveChoice.STONE) {
             System.out.println("You won!" + "\n");
-            userWins.add(1);
+            userWins++;
         } else if (userTurn == MoveChoice.SCISSOR && computerTurn == MoveChoice.STONE) {
             System.out.println("Stone breaks scissor, I won!" + "\n");
-            computerWins.add(1);
+            computerWins++;
         } else if (userTurn == MoveChoice.SCISSOR && computerTurn == MoveChoice.PAPER) {
             System.out.println("You won!" + "\n");
-            userWins.add(1);
+            userWins++;
         } else if (userTurn == MoveChoice.STONE && computerTurn == MoveChoice.PAPER) {
             System.out.println("Paper wraps around stone, I won!" + "\n");
-            computerWins.add(1);
+            computerWins++;
         } else if (userTurn == MoveChoice.STONE && computerTurn == MoveChoice.SCISSOR) {
             System.out.println("You won!" + "\n");
-            userWins.add(1);
+            userWins++;
         }
     }
 
-    private static void endGameResults(MutableInt userWins, MutableInt computerWins, MutableInt numberOfTrials) {
+    private static void endGameResults() {
         DecimalFormat df = new DecimalFormat("#.00");
-        double userWinPercentage = ((double) userWins.toInteger() / (double) numberOfTrials.toInteger()) * 100;
-        double computerWinPercentage = ((double) computerWins.toInteger() / (double) numberOfTrials.toInteger()) * 100;
+        double userWinPercentage = ((double) userWins / (double) numberOfTrials) * 100;
+        double computerWinPercentage = ((double) computerWins / (double) numberOfTrials) * 100;
         System.out.println("Number of trials: " + numberOfTrials);
         System.out.println("I won " + computerWins + "(" + df.format(computerWinPercentage) + "%). You won " + userWins + "(" + df.format(userWinPercentage) + "%).");
         System.out.println("Bye!");
     }
 }
-
-/*
-1. Kā labāk risināt šāda tipa uzdevumus, visas metodes vienā klasē (piemēram, Main), vai kā es tikko izdarīju - uztaisīt atsevišķu
-klasi? Vai arī taisīt pat vairāk klases?
-
-2. Es izmantoju MutableInt, jo tādā veidā varu saglabāt izmaiņas ar tiem mainīgajiem, kurus es izmantoju kā argumentus. Tas ir pieņemami
-vai tomēr vajadzētu atturēties no MutableInt izmantošanas? Kā es šeit ar parasto int varēju appiet, ka metodes rezultātā programma nesaglabā
-vērtību (whoWonTurn metodē).
-*/
-
